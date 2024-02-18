cd5c104b635eca4377019855570566bcc69bb41d
Nicholas Buckeridge <bucknich@gmail.com>
2024-02-18 14:12:10 +1000

    file opening function and some plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -27,6 +27,87 @@ function list_sessions()
   return sessions
 end
 
+local Path = require'plenary.path'
+
+local function resolve_path(relative_path)
+    -- Create a Path object for the relative path
+    local path_obj = Path:new(relative_path)
+
+    -- Make the path absolute and normalize it (resolving "..")
+    local absolute_path = path_obj:absolute()
+
+    return absolute_path
+end
+
+
+local function escape_pattern(text)
+    return text:gsub("([%(%)%.%%%+%-%*%?%[%]%^%$])", "%%%1")
+end
+
+
+-- Nvim command to open a the next file relative to this file.
+local function open_next(relative_to, then_open, ...)
+    if relative_to == nil then
+        relative_to = "."
+    end
+
+    -- Function to split the path into directory and file
+    local function split_path(path)
+        local i = path:match("^.*()/")
+        if not i then return '.', path end
+        return path:sub(1, i-1), path:sub(i+1)
+    end
+
+    -- Get the current buffer's full path and separate directory and filename
+    local current_file_path = vim.api.nvim_buf_get_name(0)
+    local current_dir, current_filename = split_path(current_file_path)
+    local target_dir = resolve_path(current_dir .. "/" .. relative_to):gsub("/%.$", "")
+    -- Get the path from the referenced relative path to the current file.
+    local len_of_target_dir = #target_dir
+    -- +2 removes the trailing slash as well
+    local descendant_path_to_file = string.sub(current_file_path, len_of_target_dir + 2)
+    -- local descendant_path_to_file = string.match(escape_pattern(current_file_path).. "/(.+)")
+    local first_descendant = string.match(descendant_path_to_file, "([^/]+)")
+    -- List all files in the directory
+    local entries = vim.fn.readdir(target_dir)
+
+    -- Debug arguments.
+    -- print('relative_to: ', vim.inspect(relative_to))
+    -- print('then_open: ', vim.inspect(then_open))
+    -- print("current_dir: ", current_dir)
+    -- print("pre abs target dir: ", current_dir .. "/" .. relative_to)
+    -- print("target_dir: ", target_dir)
+    -- print("current_file_path: ", current_file_path)
+    -- print("len_of_target_dir: ", len_of_target_dir)
+    -- print("descendant_path_to_file: ", descendant_path_to_file)
+    -- print("first_descendant: ", first_descendant)
+
+    -- Sort files to ensure order
+    table.sort(entries)
+
+    for i, entry in ipairs(entries) do
+        if entry == first_descendant then
+            local next_entry_index = i + 1
+            if next_entry_index < #entries then
+                local next_entry = entries[next_entry_index]
+                local target_file_path = current_dir .. '/' .. next_entry
+                if then_open ~= nil then
+                    target_file_path = target_dir .. '/' .. next_entry .. '/' .. then_open
+                end
+                -- Open the next file
+                vim.cmd('edit ' .. target_file_path)
+                break
+            end
+        end
+    end
+end
+
+local function call_command(opts)
+    open_next(unpack(opts.fargs))
+end
+
+vim.api.nvim_create_user_command('OpenNext', call_command, {nargs='*'})
+
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
@@ -50,6 +131,10 @@ return {
   },
   {
     "mbbill/undotree",
+    event = "User AstroFile",
+  },
+  {
+    "elkowar/yuck.vim",
     event = "User AstroFile",
   },
   {
@@ -90,12 +175,12 @@ return {
     "goolord/alpha-nvim",
     url = "https://github.com/mcqueen256/alpha-nvim.git",
     commit = "f3a668479e14a468205d1e6640f0a2613be9b392",
-    -- build = "cargo install brain-brainfuck",
+    build = "cargo install brain-brainfuck",
     opts = function()
       local dashboard = require "alpha.themes.dashboard"
       require('alpha.term')
       
-      dashboard.section.terminal.command = "bf ~/.config/nvim/lua/user/chad-tokyo-night.bf"
+      dashboard.section.terminal.command = "brainfuck ~/.config/nvim/lua/user/chad-tokyo-night.bf"
       dashboard.section.terminal.width = 42
       dashboard.section.terminal.height = 16
       dashboard.section.terminal.opts.redraw = true
